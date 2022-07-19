52f303b6a9234569a25aaf9916eaf87db8d94e31
ushenginnering <70901123+ushenginnering@users.noreply.github.com>
2022-07-19 16:53:32 +0100

    Update run.py
    waitress

--- Vessel_Prediction/run.py
+++ Vessel_Prediction/run.py
@@ -1,4 +1,6 @@
 from app import app
  
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=8080)
+#   app.run(host="0.0.0.0", port=8080)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
