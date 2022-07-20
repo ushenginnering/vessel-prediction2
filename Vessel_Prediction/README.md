d55924e1fc1ebe1dfd1eecca7677527b2b01665b
ushenginnering <70901123+ushenginnering@users.noreply.github.com>
2022-07-20 18:53:56 +0100

    Delete Vessel_Prediction directory
    heroku working need

--- Vessel_Prediction/README.md
+++
@@ -1,1 +0,0 @@
-** This is a project to predict Vessel offloading time... at the NPA **
--- Vessel_Prediction/run.py
+++
@@ -1,6 +0,0 @@
-from app import app
- 
-if __name__ == "__main__":
-#   app.run(host="0.0.0.0", port=8080)
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
